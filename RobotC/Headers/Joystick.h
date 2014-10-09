56e0a92c9b900fda6717c5db9714b586524bbbac
Peter Maldonado <peter.h.maldonado@gmail.com>
2014-10-09 16:18:15 -0400

    add deadband code to joystick, remove button enum

--- RobotC/Headers/Joystick.h
+++ RobotC/Headers/Joystick.h
@@ -2,20 +2,30 @@
 #define JOYSTICK_H
 #pragma systemFile
 
-typedef enum JoystickButton {
-	BUTTON_A		= 2,
-	BUTTON_B		= 3,
-	BUTTON_X		= 1,
-	BUTTON_Y		= 4,
-	BUTTON_LB		= 5,
-	BUTTON_RB		= 6,
-	BUTTON_LT		= 7,
-	BUTTON_RT		= 8,
-	BUTTON_BACK		= 9,
-	BUTTON_START	= 10,
-	BUTTON_JOYL		= 11,
-	BUTTON_JOYR		= 12,
-	BUTTON_NUM,
-} JoystickButton;
 
+
+#include "..\Libraries\Joystick.c"
 #endif // JOYSTICK_H
+
+/*
+int   Math_TrimDeadband(int input, int deadband=g_JoystickDeadband);
+float Math_TrimDeadband(float input, float deadband=g_JoystickDeadband);
+float Math_Normalize(float input, float originalMax, float newMax);
+
+int   Math_TrimDeadband(int input, int deadband) {
+	int output = 0;
+	if (abs(input)>deadband==true) {
+		output = input;
+	}
+	return output;
+}
+float Math_TrimDeadband(float input, float deadband) {
+	float output = 0;
+	if (abs(input)>deadband==true) {
+		output = input;
+	}
+	return output;
+}
+float Math_Normalize(float input, float originalMax, float newMax) {
+	return input*newMax/originalMax;
+}*/
