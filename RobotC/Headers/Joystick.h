31b36275860ca23fea6bd664c98dc3892cd47d0d
jimmyli97 <jimmy.jiajian.li@gmail.com>
2014-10-19 13:44:39 -0400

    Add skeleton Joystick.h library   Contains enums for referring to joystick buttons

--- RobotC/Headers/Joystick.h
+++ RobotC/Headers/Joystick.h
@@ -2,39 +2,71 @@
 #define JOYSTICK_H
 #pragma systemFile
 
-typedef struct JoyButtons {
+//Information on the TJoystick struct can be found in JoystickDriver.c
 
-} JoyButtons;
+//Button numbers for bitmask
+//               _==7==_                               _==8==_
+//              / __5__ \                             / __6__ \.
+//            +.-'_____'-.---------------------------.-'_____'-.+
+//           /   |  0  |  '.                       .'  |     |   \.
+//          / ___| /|\ |___ \                     / ___|(Y/4)|___ \.
+//         / |   7  |  1   | ;   __        __    ; |             | ;
+//         | |6<---   --->2| |  |__|      |__|   | |(X/1)   (B/3)| |
+//         | |___5  |  3___| ; BACK/9   START/10 ; |___       ___| ;
+//         |\    | \|/ |    /  _     ___      _   \    |(A/2)|    /|
+//         | \   |__4__|  .','" "', |___|  ,'" "', '.  |_____|  .' |
+//         |  '-.______.-' /       \ MODE /       \  '-._____.-'   |
+//         |               |       |------|       |                |
+//         |              /\       /      \       /\               |
+//         |             /  '.___.'        '.___.'  \              |
+//         |            /                            \             |
+//          \          /                              \           /
+//           \________/                                \_________/
+// NOTE: ASCII Art adapted from: http://chris.com/ascii/index.php?art=video%20games/other
 
+#include "JoystickDriver.c"
 
-//Joystick data container
-typedef struct JoyData {
-	short buttons;
-} JoyData;
-
-void Joystick_Interpret();
-
-#endif // JOYSTICK_H
-
-/*
-int   Math_TrimDeadband(int input, int deadband=g_JoystickDeadband);
-float Math_TrimDeadband(float input, float deadband=g_JoystickDeadband);
-float Math_Normalize(float input, float originalMax, float newMax);
-
-int   Math_TrimDeadband(int input, int deadband) {
-	int output = 0;
-	if (abs(input)>deadband==true) {
-		output = input;
-	}
-	return output;
+//Enum to assign button numbers meaningful names
+typedef enum joyButtons {
+	BUTTON_X = 1,
+	BUTTON_A = 2,
+	BUTTON_B = 3,
+	BUTTON_Y = 4,
+	BUTTON_LB = 5, //Left bumper
+	BUTTON_RB = 6, //Right bumper
+	BUTTON_LT = 7, //Left trigger
+	BUTTON_RT = 8, //Right trigger
+	BUTTON_BACK = 9,
+	BUTTON_START = 10,
 }
-float Math_TrimDeadband(float input, float deadband) {
-	float output = 0;
-	if (abs(input)>deadband==true) {
-		output = input;
-	}
-	return output;
+
+//Enum to assign tophat ("d-pad") numbers meaningful names
+//Uses compass directions
+typedef enum joyTophat {
+	TOPHAT_N = 0,
+	TOPHAT_NE = 1,
+	TOPHAT_E = 2,
+	TOPHAT_SE = 3,
+	TOPHAT_S = 4,
+	TOPHAT_SW = 5,
+	TOPHAT_W = 6,
+	TOPHAT_NW = 7,
 }
-float Math_Normalize(float input, float originalMax, float newMax) {
-	return input*newMax/originalMax;
-}*/
+
+//Updates ALL joystick states to last received joystick bluetooth message
+void updateJoyState() {
+	getJoystickSettings(joystick);
+}
+
+//Checks if button is pressed in joystick #(joyNum)
+//Note that joyNum will be interpreted as joystick #1 if passed 0,
+//and joystick #2 otherwise
+bool joyButtonPressed(int joyNum, joyButtons joyButton) {
+	if(joyNum) { //C interprets an int as TRUE if int != 0
+		return joy1Btn((int)joyButton); //JoystickDriver defined function
+	} else {
+		return joy2Btn((int)joyButton); //JoystickDriver defined function
+	}
+}
+
+#endif
