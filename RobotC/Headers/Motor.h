a5c52ba8127ea8b6df5970369beaffda4d9aa1dc
jimmyli97 <jimmy.jiajian.li@gmail.com>
2014-10-19 15:04:31 -0400

    Add max power array to Motor.h   Implements and fixes issue #13 by using array[tMotor]

--- RobotC/Headers/Motor.h
+++ RobotC/Headers/Motor.h
@@ -2,23 +2,42 @@
 #define MOTOR_H
 #pragma SystemFile
 
-//Number of motors
-#define NUM_MOTORS 4
+////////////////////////////////////////////////////////////////////////////////////////
+/*A note on array indexing:
+  Arrays can be accessed by converting the tMotor enum to an int to get the array
+  index. Here's the rationale for determining array size.
+  Although FTC caps the number of motors to 8 12v DC motors + 3 NXT motors,
+  RobotC caps it to kNumbOfTotalMotors. We have no idea how RobotC generates the tMotor
+  enum, so assume the worst and make the size of the array kNumbOfTotalMotors.
+*/
+////////////////////////////////////////////////////////////////////////////////////////
 
 //Constants to keep PID working and counter stalling regardless of battery level
-#define MAX_PID_POWER 75
+#define MAX_NEVEREST_POWER 78
 #define MIN_STALL_POWER 15
 
+
+//Arrays for storing power limits for each motor
+static int maxMotorPower[kNumbOfTotalMotors];
+static int minMotorPower[kNumbOfTotalMotors];
+
+//Initialize motor definitions and state
+void initMotors() {
+	//memset used here to initialize default values
+	memset(maxMotorPower, 100, sizeof(maxMotorPower));
+	memset(minMotorPower, MIN_STALL_POWER, sizeof(minMotorPower));
+}
+
 void motorSetSafePower(tMotor currentMotor, int power) {
-	if (power > MAX_PID_POWER) {
-		power = MAX_PID_POWER;
-	} else if (power < -MAX_PID_POWER) {
-		power = -MAX_PID_POWER;
-	} else if (power > -MIN_STALL_POWER && power < MIN_STALL_POWER) { //in deadband
+	int maxPower = maxMotorPower[(int)tMotor];
+	int minPower = minMotorPower[(int)tMotor];
+	if (power > maxPower) {
+		power = maxPower;
+	} else if (power < -maxPower) {
+		power = -maxPower;
+	} else if (power > -minPower && power < minPower) { //in deadband
 		power = 0;
 	}
-	power = (power > MAX_PID_POWER) ? MAX_PID_POWER : power;
-	power = (power < -MAX_PID_POWER) ? -MAX_PID_POWER : power;
 	motor[currentMotor] = power;
 }
 
