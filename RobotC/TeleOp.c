3b05d548bc62db520d5d6024f5c1faab15148798
Jimmy Li <jimmy.jiajian.li@gmail.com>
2014-11-01 15:13:31 -0400

    Add motor enum
    Fixes issue #26

--- RobotC/TeleOp.c
+++ RobotC/TeleOp.c
@@ -1,9 +1,9 @@
 #pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTServo,  none)
 #pragma config(Sensor, S1,     ,               sensorI2CMuxController)
-#pragma config(Motor,  mtr_S1_C1_1,     Motor_Mec_FR,  tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C1_2,     Motor_Mec_FL,  tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C2_1,     Motor_Mec_BR,  tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C2_2,     Motor_Mec_BL,  tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C1_1,     Michelangelo_FR,  tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C1_2,     Donatello_FL,  tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_1,     Raphael_BR,  tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_2,     Leonardo_BL,  tmotorTetrix, openLoop)
 #pragma config(Servo,  srvo_S1_C3_1,    Servo_Tube,           tServoStandard)
 #pragma config(Servo,  srvo_S1_C3_2,    servo2,               tServoNone)
 #pragma config(Servo,  srvo_S1_C3_3,    servo3,               tServoNone)
@@ -15,6 +15,15 @@
 #include "Headers\Motor.h"
 #include "Headers\Servo.h"
 #include "Headers\JoyMecanumDrive.h"
+
+//Used for reference by other functions (since #pragma configs are forced to be here)
+//They're named after TMNT because Lisa.
+typedef enum MecMotors {
+	MecMotorFL = Donatello_FL,
+	MecMotorBL = Leonardo_BL,
+	MecMotorFR = Michelangelo_FR,
+	MecMotorBR = Raphael_BR,
+}
 
 void initialize() {
 	initServos();
