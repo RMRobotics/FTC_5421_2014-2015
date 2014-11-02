dff5333819ce32fca2f3c6009abfa3b13d98074b
jimmyli97 <jimmy.jiajian.li@gmail.com>
2014-11-02 11:21:28 -0500

    Add joystick interpretation code Currently has one setting: DEADBAND Easily modifiable, so I will say that this resolves issue #20

--- RobotC/Headers/JoyMecanumDrive.h
+++ RobotC/Headers/JoyMecanumDrive.h
@@ -4,21 +4,54 @@
 
 #include "Joystick.h"
 #include "Motor.h"
+#include "Drive.h"
+
+//Joystick constants
+#define DEADBAND 10
 
 //This function interprets a TJoystick struct and sets desired motor powers.
 //This uses mecanum drive in arcade style.
 //  Left joystick controls strafing and forwards/backwards
 //  Right joystick (x-axis only) controls rotation
 void joymecdriveSetDesiredPower(DesiredMotorVals *desiredMotorVals, TJoystick *joyState) {
+	//////////////
+	//DRIVE CODE//
+	//////////////
 	int joy1y1 = joyState->joy1_y1;
 	int joy1x2 = joyState->joy1_x2;
 	int joy1x1 = joyState->joy1_x1;
-	/*
-	motorSetDesiredSafePower(MecMotorFL, joy1y1 + joy1x2 + joy1x1);
-	motorSetDesiredSafePower(MecMotorBL, joy1y1 + joy1x2 - joy1x1);
-	motorSetDesiredSafePower(MecMotorBR, joy1y1 - joy1x2 + joy1x1);
-	motorSetDesiredSafePower(MecMotorFR, joy1y1 - joy1x2 - joy1x1);
-	*/
+
+	//Due to short/int sizes they allow these to be -128 to 127, so chop off -128
+	if (joy1y1 == -128) {
+		joy1y1 = -127;
+	}
+	if (joy1x1 == -128) {
+		joy1x1 = -127;
+	}
+	if (joy1x2 == -128) {
+		joy1x2 = -127;
+	}
+
+	int angle = 0;
+	float speed = 0.0;
+	float rotation = 0.0;
+
+	//Deadband check
+	if (abs(joy1y1) >= DEADBAND || abs(joy1x1) >= DEADBAND || abs(joy1x2) >= DEADBAND) {
+		//We use atan2 because we want the actual angle, not the reference angle
+		angle = (int) radiansToDegrees((atan2(joy1y1,joy1x1)));
+		speed = sqrt((long)(pow(joy1x1,2) + pow(joy1y1,2)));
+		rotation = ((float) joy1x2) / 127.0;
+		driveSetMecMotorPolarDegrees(desiredMotorVals, angle, speed, rotation);
+	} else {
+	  driveZeroMecMotor(desiredMotorVals);
+	}
+
+	/////////
+	//Other//
+	/////////
+
+
 }
 
 #endif
