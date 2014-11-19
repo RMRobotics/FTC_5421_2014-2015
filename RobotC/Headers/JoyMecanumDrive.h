cfa6cd8487c65fd55459cb743b735b6a7437020e
Peter Maldonado <peterhmaldonado@gmail.com>
2014-11-18 19:28:18 -0500

    Add dPad drive (#19), add constrain to speed/rot

--- RobotC/Headers/JoyMecanumDrive.h
+++ RobotC/Headers/JoyMecanumDrive.h
@@ -35,18 +35,51 @@ void joymecdriveSetDesiredPower(DesiredMotorVals *desiredMotorVals, TJoystick *j
 	int angle = 0;
 	float speed = 0.0;
 	float rotation = 0.0;
-	eraseDisplay();
-	//Deadband check
-	if (abs(joy1y1) >= DEADBAND || abs(joy1x1) >= DEADBAND || abs(joy1x2) >= DEADBAND) {
-		//We use atan2 because we want the actual angle, not the reference angle
-		angle = (int) radiansToDegrees((atan2(joy1x1,joy1y1)));
+	JoyTophat tophat = joyGetTophat(joyState);
+	switch (tophat) {
+		case TOPHAT_N:
+			driveSetMecMotorPolarDegrees(desiredMotorVals, 0, 1.0, 0.0);
+			break;
+		case TOPHAT_NE:
+			driveSetMecMotorPolarDegrees(desiredMotorVals, 45, 1.0, 0.0);
+			break;
+		case TOPHAT_E:
+			driveSetMecMotorPolarDegrees(desiredMotorVals, 90, 1.0, 0.0);
+			break;
+		case TOPHAT_SE:
+			driveSetMecMotorPolarDegrees(desiredMotorVals, 135, 1.0, 0.0);
+			break;
+		case TOPHAT_S:
+			driveSetMecMotorPolarDegrees(desiredMotorVals, 180, 1.0, 0.0);
+			break;
+		case TOPHAT_SW:
+			driveSetMecMotorPolarDegrees(desiredMotorVals, 225, 1.0, 0.0);
+			break;
+		case TOPHAT_W:
+			driveSetMecMotorPolarDegrees(desiredMotorVals, 270, 1.0, 0.0);
+			break;
+		case TOPHAT_NW:
+			driveSetMecMotorPolarDegrees(desiredMotorVals, 315, 1.0, 0.0);
+			break;
+		default:
+			//Give up to joystick
+			//Deadband check
+			if (abs(joy1y1) >= DEADBAND || abs(joy1x1) >= DEADBAND || abs(joy1x2) >= DEADBAND) {
+				//We use atan2 because we want the actual angle, not the reference angle
+				angle = (int) radiansToDegrees((atan2(joy1x1,joy1y1)));
+				speed = sqrt((long)(pow(joy1x1,2) + pow(joy1y1,2))) / 127.0;
+				rotation = ((float) joy1x2) / 127.0;
+				speed = constrain(speed,-1.0,1.0);
+				rotation = constrain(rotation,-1.0,1.0);
+				driveSetMecMotorPolarDegrees(desiredMotorVals, angle, speed, rotation);
+			} else {
+				driveZeroMecMotor(desiredMotorVals);
+			}
+			break;
+	}//switch tophat
 
-		speed = sqrt((long)(pow(joy1x1,2) + pow(joy1y1,2))) / 127.0;
-		rotation = ((float) joy1x2) / 127.0;
-		driveSetMecMotorPolarDegrees(desiredMotorVals, angle, speed, rotation);
-		} else {
-		driveZeroMecMotor(desiredMotorVals);
-	}
+	nxtDisplayString(3, "R:%f %f %f %f",  ((float)joy1x2), rotation);
+	nxtDisplayString(5, "P: %d %d %d", (int)(angle*100),(int)(speed*100),(int)(rotation*100));
 
 	/////////
 	//Other//
