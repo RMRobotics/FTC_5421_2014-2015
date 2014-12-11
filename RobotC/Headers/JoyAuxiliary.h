077adc1e4a7ad4936dc503bc97ce4012dfb4a6ee
Joshua Chen <chen.joshua.2017@gmail.com>
2014-12-11 17:03:26 -0500

    Complete joy auxiliary

--- RobotC/Headers/JoyAuxiliary.h
+++ RobotC/Headers/JoyAuxiliary.h
@@ -6,10 +6,17 @@
 #include "Drive.h"
 
 
-
 void joyChuteMotors(DesiredMotorVals *desiredMotorVals, TJoystick *joyState){
-	if (joystick.joy1_Buttons == 8){
-		motor[chuteLeft] = 60;
-		motor[chuteRight] = 60;
+	bool chuteVacuum = joyButtonPressed(joyState, JOY1, BUTTON_RT)
+	bool chuteEject = joyButtonPressed(joyState, JOY1, BUTTON_LT)
+	if (chuteVacuum == true){
+		desiredMotorVals->power[Chute_Left] = 50;
+		desiredMotorVals->power[Chute_Right] = 50;
+	}else if (chuteEject == true){
+		desiredMotorVals->power[Chute_Left] = -50;
+		desiredMotorVals->power[Chute_Right] = -50;
+	}else {
+		desiredMotorVals->power[Chute_Left] = 0;
+		desiredMotorVals->power[Chute_Right] = 0;
 	}
 }
