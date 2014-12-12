3d388d01ae32cecec0d5b3c7084beaf53fe8c4e4
Joshua Chen <chen.joshua.2017@gmail.com>
2014-12-12 17:12:11 -0500

    Add linear slide code and add function to add all motor functions

--- RobotC/Headers/JoyAuxiliary.h
+++ RobotC/Headers/JoyAuxiliary.h
@@ -7,8 +7,8 @@
 
 
 void joyChuteMotors(DesiredMotorVals *desiredMotorVals, TJoystick *joyState){
-	bool chuteVacuum = joyButtonPressed(joyState, JOY1, BUTTON_RT)
-	bool chuteEject = joyButtonPressed(joyState, JOY1, BUTTON_LT)
+	bool chuteVacuum = joyButtonPressed(joyState, JOY1, BUTTON_RT);
+	bool chuteEject = joyButtonPressed(joyState, JOY1, BUTTON_LT);
 	if (chuteVacuum == true){
 		desiredMotorVals->power[Chute_Left] = 50;
 		desiredMotorVals->power[Chute_Right] = 50;
@@ -22,8 +22,8 @@ void joyChuteMotors(DesiredMotorVals *desiredMotorVals, TJoystick *joyState){
 }
 
 void joySlideMotors(DesiredMotorVals *desiredMotorVals, TJoystick *joyState){
-	bool slidesUp = joyButtonPressed(joyState, JOY2, BUTTON_RT)
-	bool slidesDown = joyButtonPressed(joyState, JOY2, BUTTON_LT)
+	bool slidesUp = joyButtonPressed(joyState, JOY2, BUTTON_RT);
+	bool slidesDown = joyButtonPressed(joyState, JOY2, BUTTON_LT);
 	if (slidesUp == true){
 		desiredMotorVals->power[Slides_Left] = 50;
 		desiredMotorVals->power[Slides_Right] = 50;
--- RobotC/TeleOp.c
+++ RobotC/TeleOp.c
@@ -2,14 +2,14 @@
 #pragma config(Hubs,  S4, HTMotor,  HTMotor,  HTServo,  none)
 #pragma config(Sensor, S1,     ,               sensorI2CMuxController)
 #pragma config(Sensor, S4,     ,               sensorI2CMuxController)
-#pragma config(Motor,  mtr_S1_C1_1,     Chute_Right,   tmotorTetrix, PIDControl, encoder)
-#pragma config(Motor,  mtr_S1_C1_2,     motorE,        tmotorTetrix, PIDControl, encoder)
+#pragma config(Motor,  mtr_S1_C1_1,     Chute_Right,   tmotorTetrix, PIDControl, reversed, encoder)
+#pragma config(Motor,  mtr_S1_C1_2,     Slides_Right,  tmotorTetrix, PIDControl, encoder)
 #pragma config(Motor,  mtr_S1_C2_1,     Michelangelo_FR, tmotorTetrix, PIDControl, reversed, encoder)
 #pragma config(Motor,  mtr_S1_C2_2,     Raphael_BR,    tmotorTetrix, PIDControl, reversed, encoder)
 #pragma config(Motor,  mtr_S4_C1_1,     Donatello_FL,  tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S4_C1_2,     Leonardo_BL,   tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S4_C2_1,     Chute_Left,    tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S4_C2_2,     motorK,        tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S4_C2_2,     Slides_Left,   tmotorTetrix, openLoop)
 #pragma config(Servo,  srvo_S4_C3_1,    KickStand,            tServoStandard)
 #pragma config(Servo,  srvo_S4_C3_2,    servo2,               tServoNone)
 #pragma config(Servo,  srvo_S4_C3_3,    servo3,               tServoNone)
@@ -22,6 +22,7 @@
 #include "Headers\Servo.h"
 #include "Headers\JoyMecanumDrive.h"
 #include "Headers\Global.h"
+#include "Headers\JoyAuxiliary.h"
 
 //Stores desired motor values
 DesiredMotorVals desiredMotorVals;
@@ -34,6 +35,11 @@ void initialize() {
 	memset(&desiredMotorVals, 0, sizeof(desiredMotorVals));
 }
 
+void callAuxiliaryMotors(){
+	joyChuteMotors(&desiredMotorVals, joyGetJoystickPointer());
+	joySlideMotors(&desiredMotorVals, joyGetJoystickPointer());
+}
+
 task main() {
 	initialize();
 	waitForStart();
@@ -42,6 +48,7 @@ task main() {
 		joymecdriveSetDesiredPower(&desiredMotorVals, joyGetJoystickPointer());
 		//driveSetMecMotorPolarDegrees(&desiredMotorVals, 90, 1.0, 1.0);
 		motorSetActualPowerToDesired(&desiredMotorVals);
+		callAuxiliaryMotors();
 		writeDebugStreamLine("------");
 	}
 }
