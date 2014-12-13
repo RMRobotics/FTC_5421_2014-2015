1ba7da16526998512102cf10ae25dca96112a652
Joshua Chen <chen.joshua.2017@gmail.com>
2014-12-13 15:02:36 -0500

    Added auxiliary functions for the servos /n (tube grab and kick stand)

--- RobotC/Headers/JoyAuxiliary.h
+++ RobotC/Headers/JoyAuxiliary.h
@@ -9,10 +9,10 @@
 void joyChuteMotors(DesiredMotorVals *desiredMotorVals, TJoystick *joyState){
 	bool chuteVacuum = joyButtonPressed(joyState, JOY1, BUTTON_RT);
 	bool chuteEject = joyButtonPressed(joyState, JOY1, BUTTON_LT);
-	if (chuteVacuum == true){
+	if (chuteVacuum){
 		desiredMotorVals->power[Chute_Left] = 50;
 		desiredMotorVals->power[Chute_Right] = 50;
-	}else if (chuteEject == true){
+	}else if (chuteEject){
 		desiredMotorVals->power[Chute_Left] = -50;
 		desiredMotorVals->power[Chute_Right] = -50;
 	}else {
@@ -24,14 +24,34 @@ void joyChuteMotors(DesiredMotorVals *desiredMotorVals, TJoystick *joyState){
 void joySlideMotors(DesiredMotorVals *desiredMotorVals, TJoystick *joyState){
 	bool slidesUp = joyButtonPressed(joyState, JOY2, BUTTON_RT);
 	bool slidesDown = joyButtonPressed(joyState, JOY2, BUTTON_LT);
-	if (slidesUp == true){
+	if (slidesUp){
 		desiredMotorVals->power[Slides_Left] = 50;
 		desiredMotorVals->power[Slides_Right] = 50;
-	}else if (slidesDown == true){
+	}else if (slidesDown){
 		desiredMotorVals->power[Slides_Left] = -50;
 		desiredMotorVals->power[Slides_Right] = -50;
 	}else {
 		desiredMotorVals->power[Slides_Left] = 0;
 		desiredMotorVals->power[Slides_Right] = 0;
+	}
+}
+
+void joyTubeGrabber(DesiredMotorVals *desiredMotorVals, TJoystick *joyState){
+	bool tubeRelease = joyButtonPressed(joyState, JOY2, BUTTON_RB);
+	bool tubeGrab = joyButtonPressed(joyState, JOY2, BUTTON_LB);
+	if (tubeRelease){
+		servoSetNonCont(TubeGrabber, 0);
+	}else if (tubeGrab){
+		servoSetNonCont(TubeGrabber, 100);
+	}
+}
+
+void joyKickStand(DesiredMotorVals *desiredMotorVals, TJoystick *joyState){
+	bool kickStandGrab = joyButtonPressed(joyState, JOY1, BUTTON_RB);
+	bool kickStandRelease = joyButtonPressed(joyState, JOY1, BUTTON_LB);
+	if (kickStandGrab){
+		servoSetNonCont(KickStand, 145);
+	}else if (kickStandRelease){
+		servoSetNonCont(KickStand, 3);
 	}
 }
