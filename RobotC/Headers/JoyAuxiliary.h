80f357513818d3694e0c6c63623ac89e2623b0e4
Joshua Chen <chen.joshua.2017@gmail.com>
2015-01-13 13:51:20 -0500

    Update code for wings and ultrasonic servo

--- RobotC/Headers/JoyAuxiliary.h
+++ RobotC/Headers/JoyAuxiliary.h
@@ -8,19 +8,55 @@
 
 
 void joyLift(DesiredMotorVals *desiredMotorVals, TJoystick *joyState){
-	if (joyButtonPressed(joyState, JOY1, BUTTON_A)) {
+	if (joyButtonPressed(joyState, JOY1, BUTTON_Y)) {
 		desiredMotorVals->power[Lift] = 50;
-	} else if (joyButtonPressed(joyState, JOY1, BUTTON_B)) {
+	} else if (joyButtonPressed(joyState, JOY1, BUTTON_RB)) {
 		desiredMotorVals->power[Lift] = -50;
 	} else {
 		desiredMotorVals->power[Lift] = 0;
 	}
 }
 
+
+bool centerWingIsMoving = false;
+bool centerWingDown = false;
+int centerWingPulseTimeMs = 200;
+
+void joyWing(DesiredMotorVals *desiredMotorVals, TJoystick *joyState) {
+	if (centerWingIsMoving) {
+		if (time1[T1] > centerWingPulseTimeMs) {
+			desiredMotorVals->power[Wing_Middle] = 0;
+			centerWingIsMoving = false;
+			centerWingDown = !centerWingDown;
+		}
+	} else {
+		if (joyButtonPressed(joyState, JOY1, BUTTON_X)) {
+			ClearTimer(T1);
+			centerWingIsMoving = true;
+			if (centerWingDown) {
+				desiredMotorVals->power[Wing_Middle] = 75;
+			} else {
+				desiredMotorVals->power[Wing_Middle] = -75;
+			}
+		}
+	}
+	if (joyButtonPressed(joyState, JOY1, BUTTON_B)) {
+		servoSetNonCont(Wing_Base, servoDefinitions[Wing_Base].maxValue);
+	} else if (joyButtonPressed(joyState, JOY1, BUTTON_A)) {
+		servoSetNonCont(Wing_Base, servoDefinitions[Wing_Base].minValue);
+	}
+}
+
+void joyHarvester(DesiredMotorVals *desiredMotorVals, TJoystick *joyState) {
+	if (joyButtonPressed(joyState, JOY1, BUTTON_LT)) {
+		desiredMotorVals->power[Harvester] = 50;
+	} else if (joyButtonPressed(joyState, JOY1, BUTTON_RT)) {
+		desiredMotorVals->power[Harvester] = -50;
+	} else {
+		desiredMotorVals->power[Harvester] = 0;
+	}
+}
+
 void joyAuxDebug(DesiredMotorVals *desiredMotorVals, TJoystick *joyState) {
-	writeDebugStream("Left Slide Motor: %d\n", motor[Slides_Left]);
-	writeDebugStream("Right Slide Motor: %d\n", motor[Slides_Right]);
-	writeDebugStream("Left Slide Motor Desired: %d\n", desiredMotorVals->power[Slides_Left]);
-	writeDebugStream("Right Slide Motor Desired: %d\n", desiredMotorVals->power[Slides_Right]);
 }
 #endif
